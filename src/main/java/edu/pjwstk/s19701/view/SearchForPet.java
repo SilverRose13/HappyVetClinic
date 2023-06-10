6956334dc85e3277837ab2cc1b13959417a96ddf
SilverRose13 <49096824+SilverRose13@users.noreply.github.com>
2023-06-10 08:57:22 +0200

    Panel do szukania

---
+++ src/main/java/edu/pjwstk/s19701/view/SearchForPet.java
@@ -0,0 +1,20 @@
+package edu.pjwstk.s19701.view;
+
+import javax.swing.*;
+
+public class SearchForPet {
+    private JButton searchByChipButton;
+    private JTextField nameTextField;
+    private JTextField surnameTextField;
+    private JButton searchByOwnerButton;
+    private JLabel searchByChipLabel;
+    private JTextField chipNumberTextField;
+    private JLabel searchByOwnerLabel;
+    private JLabel nameLabel;
+    private JLabel surnameLabel;
+    private JLabel searchForPetLabel;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
