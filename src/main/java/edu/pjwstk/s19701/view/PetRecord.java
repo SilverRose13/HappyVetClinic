05350a2bbec04e9f0b2c1663efff2a82578ae7fe
SilverRose13 <49096824+SilverRose13@users.noreply.github.com>
2023-06-10 11:51:07 +0200

    PetRecord window

---
+++ src/main/java/edu/pjwstk/s19701/view/PetRecord.java
@@ -0,0 +1,11 @@
+package edu.pjwstk.s19701.view;
+
+import javax.swing.*;
+
+public class PetRecord {
+    private JList<JButton> patientInformation;
+    private JButton searchButton;
+    private JButton addVisitButton;
+    private JButton backButton;
+    private JComboBox comboBox1;
+}
